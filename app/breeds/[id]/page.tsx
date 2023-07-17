89152a8f38ef41f23756dc83dc0d6b3723826ae5
mastercodingbear <mastercodingbear@gmail.com>
2023-07-17 12:12:01 +0300

    fix: improve async requests using `Promise.all()`

--- app/breeds/[id]/page.tsx
+++ app/breeds/[id]/page.tsx
@@ -2,8 +2,7 @@ import { getCatBreedById, getCatImages } from '@/api/cat'
 import CatBreed from '@/app/components/breeds/CatBreed'
 
 const BreedPage = async ({ params }: { params: { id: string } }) => {
-  const breed = await getCatBreedById(params.id)
-  const images = await getCatImages(params.id)
+  const [breed, images] = await Promise.all([getCatBreedById(params.id), getCatImages(params.id)])
 
   return (
     <div className="w-full">
